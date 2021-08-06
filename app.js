64eca333ebb052dfab4bc3d47fe5538fe233b3dd
Ian Dewsbury <iandewsbury@Ians-MacBook-Pro.local>
2021-08-05 21:26:21 -0400

    Integrate nunjucks with express

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ const nunjucks = require( 'nunjucks' )
 
 app.use(volleyball)
 
-app.get('/', (req, res) => {
-    res.send('Welcome to twitter-js')
-})
-
-app.get('/news', (req, res) => {
-    res.send('Your news feed')
-})
+app.set('view engine', 'html') // have res.render work with html files
+app.engine('html', nunjucks.render) // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure( 'views', { noCache: true } ) // point nunjucks to the proper directory for templates
 
 const users = {
     title: 'Example template',
@@ -22,6 +18,15 @@ const users = {
         { name: 'Ron' }
     ]
 }
+
+app.get('/', (req, res) => {
+    res.render( 'index', users )
+})
+
+app.get('/news', (req, res) => {
+    res.send('Your news feed')
+})
+
 nunjucks.configure('views', { noCache: true })
 nunjucks.render('index.html', users, ( err, output) => {
     console.log(output)
