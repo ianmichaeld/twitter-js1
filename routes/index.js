5738f3901ddadcf741b5caa31174a2f7f2822b60
Ian Dewsbury <iandewsbury@Ians-MacBook-Pro.local>
2021-09-21 14:17:29 -0400

    Add create route form to individual user page

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,14 @@ router.get('/', ( req, res ) => {
 router.get('/users/:name', ( req, res ) => {
     let name = req.params.name;
     let list = tweetBank.find( { name: name } )
-    res.render( 'index', { tweets: list } )
-  });
+    res.render( 'index', { tweets: list, name: name, showForm: true } )
+  })
 
 router.get('/tweets/:id', ( req, res ) => {
     let id = +req.params.id;
     let list = tweetBank.find( { id: id } )
     res.render( 'index', { tweets: list } )
-  });
+  })
 
   router.post('/tweets', ( req, res)  => {
       const [ name, text ] = [ req.body.name, req.body.text ]
