e51ca9d067d4a6bef13ae29a5b9e884e0e216e3c
Ian Dewsbury <iandewsbury@Ians-MacBook-Pro.local>
2021-09-21 14:00:42 -0400

    Add route for creating tweets

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,25 @@ router.use(express.static('public'))
 
 router.get('/', ( req, res ) => {
     let tweets = tweetBank.list()
-    res.render( 'index', { tweets: tweets } )
+    res.render( 'index', { tweets: tweets, showForm: true } )
 })
 
 router.get('/users/:name', ( req, res ) => {
     let name = req.params.name;
-    let list = tweetBank.find( { name: name } );
-    res.render( 'index', { tweets: list } );
+    let list = tweetBank.find( { name: name } )
+    res.render( 'index', { tweets: list } )
   });
 
 router.get('/tweets/:id', ( req, res ) => {
     let id = +req.params.id;
-    let list = tweetBank.find( { id: id } );
-    res.render( 'index', { tweets: list } );
+    let list = tweetBank.find( { id: id } )
+    res.render( 'index', { tweets: list } )
   });
+
+  router.post('/tweets', ( req, res)  => {
+      const [ name, text ] = [ req.body.name, req.body.text ]
+      tweetBank.add( name, text )
+      res.redirect('/')
+  })
 
 module.exports = router
