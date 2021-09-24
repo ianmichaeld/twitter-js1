9e74dd907854ed79bc1696329bf1d4f74f8917a2
Ian Dewsbury <iandewsbury@Ians-MacBook-Pro.local>
2021-09-24 12:23:01 -0400

    formatting

--- app.js
+++ app.js
@@ -4,19 +4,21 @@ const port = 3000
 const volleyball = require( 'volleyball' )
 const bodyParser = require( 'body-parser' )
 const nunjucks = require( 'nunjucks' )
-const routes = require('./routes')
+const routes = require( './routes' ) 
 
 app.use(volleyball)
 
-app.use(bodyParser.urlencoded( {extended: false } ))
-app.use(bodyParser.json())
+app.use( bodyParser.urlencoded( {extended: false } ) )
+app.use( bodyParser.json() )
 
-app.set('view engine', 'html') // have res.render work with html files
-app.engine('html', nunjucks.render) // when giving html files to res.render, tell it to use nunjucks
+app.set( 'view engine', 'html' ) // have res.render work with html files
+app.engine( 'html', nunjucks.render ) // when giving html files to res.render, tell it to use nunjucks
 nunjucks.configure( 'views', { noCache: true } ) // point nunjucks to the proper directory for templates
 
-app.use('/', routes)
+app.use( '/', routes )
 
-app.listen(port, () => {
-    console.log(`twitter-js app listening at http://localhost:${port}`)
+const server = app.listen(port, () => {
+    console.log( `twitter-js app listening at http://localhost:${port}` )
 })
+
+const io = require( 'socket.io' )( server ) 
